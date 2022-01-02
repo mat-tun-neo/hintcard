68d91b3950ee1270986d7c8b8d1882932975f638
mat-tun-neo <nmatsuo0227@yahoo.co.jp>
2022-01-02 17:51:11 +0900

    20220102_1751

--- game_main/index.php
+++ game_main/index.php
@@ -21,6 +21,7 @@
 <script src="./js/phina.min.js"></script>
 <script src="./js/axios.min.js"></script>
 <script src="./js/main.js?<?php echo date('YmdHis') ?>"></script>
+<script src="./js/server.js?<?php echo date('YmdHis') ?>"></script>
 <script src="./js/SceneMain.js?<?php echo date('YmdHis') ?>"></script>
 <script src="./js/SceneExit.js?<?php echo date('YmdHis') ?>"></script>
 <script src="./js/SpriteBase.js?<?php echo date('YmdHis') ?>"></script>
---
+++ game_main/js/server.js
@@ -0,0 +1,5 @@
+// グローバルに展開
+phina.globalize();
+
+// URL
+const HREF = "http://192.168.11.21:8080";
